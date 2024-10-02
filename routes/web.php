60ac0e39c6dfa41511bdccdd936f994449b6342a
Amel Achour <amelacho3@gmail.com>
2024-10-02 13:53:38 +0100

    remove add centre

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,5 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth'])->group(function () {
     Route::get('/categories', [CategoryController::class, 'index'])->name('CentreRecyclage.categorie');
     Route::get('/CentreRecyclage/{id}', [CategoryController::class, 'show'])->name('CentreRecyclage.show');
-    Route::get('/CentreRecyclage', [RecyclingCenterController::class, 'create'])->name('CentreRecyclage.create');
     Route::post('/CentreRecyclage', [RecyclingCenterController::class, 'store'])->name('CentreRecyclage.store');
 });
